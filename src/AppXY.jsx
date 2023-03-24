1d4dac25f13b0318aea0207a57003987fc3c7ef5
namdaeun <nde40345@gmail.com>
2023-03-25 01:13:18 +0900

    마우스 따라 가기 - 개선하기

--- src/AppXY.jsx
+++ src/AppXY.jsx
@@ -3,19 +3,16 @@ import { useState } from 'react';
 import './AppXY.css';
 
 export default function AppXY() {
-    const [x, setX] = useState(0);
-    const [y, setY] = useState(0);
-
+    const [position, setPosition] = useState({x: 0, y: 0, z: 0});
     return (
         <div className='container' 
         onPointerMove={(e) => {
             console.log(e.clientX, e.clientY);
-            setX(e.clientX);
-            setY(e.clientY);
+            setPosition(prev => ({...prev, x:e.clientX}));
         }}> 
             <div className='pointer' 
-            style={{transform: `translate(${x}px, 
-            ${y}px)`}}/>
+            style={{transform: `translate(${position.x}px, 
+            ${position.y}px)`}}/>
         </div> 
     );
 }
